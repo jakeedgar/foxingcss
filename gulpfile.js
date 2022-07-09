890ef91c82c234ed95d9213468312ae277267177
Jake Edgar <jakeedgar1012@gmail.com>
2022-07-09 05:58:43 -0700

    build out file structure

--- gulpfile.js
+++ gulpfile.js
@@ -3,13 +3,13 @@ const { src, dest, watch, series } = require('gulp')
 const sass = require('gulp-sass')(require('sass'));
 
 function buildStyles() {
-  return src('index.scss')
+  return src('src/**/*.scss')
     .pipe(sass())
     .pipe(dest('css'))
 }
 
 function watchTask() {
-  watch(['index.scss'], buildStyles)
+  watch(['src/**/*.scss'], buildStyles)
 }
 
 exports.default = series(buildStyles, watchTask)
