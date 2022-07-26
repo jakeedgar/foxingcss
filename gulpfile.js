1a54da2813f9516c1c35d040312ffc744b3946c3
Jake Edgar <jakeedgar1012@gmail.com>
2022-07-25 21:28:54 -0700

    add components content and more utilities

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,18 @@
 
 const { src, dest, watch, series } = require('gulp')
 const sass = require('gulp-sass')(require('sass'));
+// const purgecss = require('gulp-purgecss');
+
 
 function buildStyles() {
   return src('src/**/*.scss')
     .pipe(sass())
+    // .pipe(purgecss({ content: ['*.html'] }))
     .pipe(dest('css'))
 }
 
 function watchTask() {
-  watch(['src/**/*.scss'], buildStyles)
+  watch(['src/**/*.scss', '*.html'], buildStyles)
 }
 
 exports.default = series(buildStyles, watchTask)
